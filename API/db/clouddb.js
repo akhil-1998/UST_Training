5b7e5e1dadc6b38350b1e2f819a8afa0b2368d4e
Akhil M Anil <70442264+akhil-1998@users.noreply.github.com>
2021-01-14 12:55:11 +0530

    Add files via upload

---
+++ API/db/clouddb.js
@@ -0,0 +1,14 @@
+// /**
+//  * Setup the Database URL
+//  */
+
+const DB_USER = "akhil";
+const DB_PASSWORD = "ust123456";
+const DB_NAME = "acmetravel";
+const CLUSTER_HOST = "apidemo.7z7s3.mongodb.net";
+
+//DB Connection String
+//mongodb+srv://akhil:<password>@apidemo.7z7s3.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+exports.DB_URI=`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
